cf616959c55a28e4b31eaf2c2320b33e0cf3ef5b
Ashwin Kumar S <iamdevsofttech@gmail.com>
2025-11-05 04:38:33 +0000

    this is the logo and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { SiteHeader } from '@/components/site-header';
 import { SiteFooter } from '@/components/site-footer';
 import { WhatsAppButton } from '@/components/whatsapp-button';
 
-const SvgFavicon = "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 88' fill='none'%3e%3cpath d='M50 0L0 88H16L24.4648 74H75.5352L84 88H100L50 0ZM50 29.28L62.0656 60H37.9344L50 29.28Z' fill='hsl(0 72% 51%)'/%3e%3c/svg%3e";
+const SvgFavicon = "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 108 88' fill='none'%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M51.543 0.707107L0 88H16.7332L30.1332 65.8115H64.0958L51.543 42.1885L39.2668 65.8115H33.101L51.543 31.8115L69.985 65.8115H77.8668L91.2668 88H108L56.457 0.707107L51.543 0.707107ZM77.8668 65.8115H64.0958L51.543 42.1885L61.0108 24.477L102.733 88H86.8332L77.8668 65.8115Z' fill='hsl(0 72% 51%)'/%3e%3c/svg%3e";
 
 export const metadata: Metadata = {
   title: 'Ashvanth Technologies | Expert Laptop Service in Chennai & Medavakkam',
--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -2,12 +2,14 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
-      viewBox="0 0 100 88"
+      viewBox="0 0 108 88"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M50 0L0 88H16L24.4648 74H75.5352L84 88H100L50 0ZM50 29.28L62.0656 60H37.9344L50 29.28Z"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M51.543 0.707107L0 88H16.7332L30.1332 65.8115H64.0958L51.543 42.1885L39.2668 65.8115H33.101L51.543 31.8115L69.985 65.8115H77.8668L91.2668 88H108L56.457 0.707107L51.543 0.707107ZM77.8668 65.8115H64.0958L51.543 42.1885L61.0108 24.477L102.733 88H86.8332L77.8668 65.8115Z"
         fill="currentColor"
       />
     </svg>
